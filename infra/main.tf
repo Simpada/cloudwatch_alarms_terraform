4f6e15e41011dea106b462cc1515401bc094e5ce
Simpada <marcus1sp2@gmail.com>
2023-10-31 16:27:56 +0000

    added workflow and module

--- infra/main.tf
+++ infra/main.tf
@@ -45,3 +45,9 @@ resource "aws_cloudwatch_dashboard" "main" {
 }
 DASHBOARD
 }
+
+module "alarm" {
+  source = "./alarm_module"
+  alarm_email = var.alarm_email
+  prefix = var.student_name
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,9 @@
 variable "student_name" {
   type = string
+  default = "mama014"
+}
+
+variable "alarm_email" {
+  type = string
+  default = "marcus1sp2@gmail.com"
 }
